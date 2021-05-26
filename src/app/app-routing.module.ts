e181350bcb2b44b401fdae8d64cd34b970a80288
admin <aditya>
2021-05-26 08:55:57 +0530

    githubpage4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HomeComponent } from './home/home.component';
-// import {UploadComponent} from './upload/upload.component';
+import {UploadComponent} from './upload/upload.component';
 // import {BaseScenarioComponent} from './base-scenario/base-scenario.component';
 // import {NewScenarioComponent} from './new-scenario/new-scenario.component';
 // import {CompareScenarioComponent} from './compare-scenario/compare-scenario.component';
@@ -12,7 +12,7 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   
-  { path: 'upload', component:SidebarComponent},
+  { path: 'upload', component:UploadComponent},
   { path: 'baseScenario',component:SidebarComponent},
   { path: 'newScenario', component:SidebarComponent},
   { path: 'compareScenario', component:SidebarComponent},
